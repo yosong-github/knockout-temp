ccb0b53d89330581c2ee09eaa35209e2572ecc75
yosong <2404559603@qq.com>
2025-01-20 17:50:30 +0800

    chore: init

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,24 @@
 import ko from 'knockout'
 
 const Home = node => {
-  console.log('home init')
 
   node.innerHTML = require('!!raw-loader!./index.html').default.replace('<!DOCTYPE html>', '')
 
+  const data = {
+    home: ko.observable('home'),
+    changeHome: () => {
+      Promise.resolve().then(() => {
+        data.home('changed home' + new Date().getTime())
+      })
+    }
+  }
+
   ko.applyBindings(
-    {
-      home: ko.observable('home')
-    },
+    data,
     document.querySelector('#home')
   )
+
+
 }
 
 export default Home
